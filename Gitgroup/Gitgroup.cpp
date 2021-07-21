fe758665e9f5269d7a945c69cd87ef4517b7e6ba
maks555tw <maks55tw@gmail.com>
2021-07-21 20:16:34 +0300

    not?

--- Gitgroup/Gitgroup.cpp
+++ Gitgroup/Gitgroup.cpp
@@ -19,7 +19,6 @@ using namespace std;
         virtual void Nick() = 0;
     };
 
-
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
